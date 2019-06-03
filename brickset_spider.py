86b7b2b5f5a99b43abcffd6b70c7e94004701aa7
tr33rt <35830620+tr33rt@users.noreply.github.com>
2019-06-03 23:51:33 +0000

    Update brickset_spider.py

--- brickset_spider.py
+++ brickset_spider.py
@@ -8,31 +8,28 @@ import csv
 class BrickSetSpider(scrapy.Spider):
     name = "brickset_spider"
     start_urls = ['http://brickset.com/sets/year-2018']
-    f = csv.writer(open('2018_brickset_info.csv', 'w'))
-    f.writerow(['Name','Pieces','Minifigs','Image'])
     
     # Define a function to parse the HTML data for relevent info
     def parse(self, response):
         SET_SELECTOR = '.set'
+        f = csv.writer(open('2018_brickset_info.csv', 'a'))
+        f.writerow(['Name','Pieces','Minifigs','Image'])
         
         for brickset in response.css(SET_SELECTOR):
 
             NAME_SELECTOR = 'h1 a ::text'
             PIECES_SELECTOR = './/dl[dt/text() = "Pieces"]/dd/a/text()'
             MINIFIGS_SELECTOR = './/dl[dt/text() = "Minifigs"]/dd[2]/a/text()'
-            #PRICE_SELECTOR = './/dl[dt/text() = "RRP"]dd/text()'
             IMAGE_SELECTOR =  'img ::attr(src)'
           
             name = brickset.css(NAME_SELECTOR).extract(),
             pieces = brickset.xpath(PIECES_SELECTOR).extract_first(),
             minifigs = brickset.xpath(MINIFIGS_SELECTOR).extract_first(),
-            #price = brickset.xpath(PRICE_SELECTOR).extract_first(),
             image = brickset.css(IMAGE_SELECTOR).extract_first()
             
             print(name)
             print(pieces)
             print(minifigs)
-            #print(price)
             print(image)
             f.writerow([name, pieces, minifigs, image])
 
